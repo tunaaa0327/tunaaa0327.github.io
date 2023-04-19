fcdad172e29125d42c016b7f3870925c188b6511
myCode <jules27jules@gmail.con>
2023-04-19 18:24:27 +0800

    First Commit

---
+++ sample.js
@@ -0,0 +1,7 @@
+function showInputs() {
+    document.getElementById("luvu").style.display = 'block';
+    var input1 = document.getElementById('input1').value;
+    var output = document.getElementById('output');
+    output.innerHTML = input1;
+    
+}
