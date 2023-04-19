3f1ea945750a9b2240171dccbb4a213fdd9e10d9
myCode <jules27jules@gmail.con>
2023-04-19 20:33:16 +0800

    added music

--- sample.js
+++ sample.js
@@ -3,5 +3,14 @@ function showInputs() {
     var input1 = document.getElementById('input1').value;
     var output = document.getElementById('output');
     output.innerHTML = input1;
+    startMusic();
     //sample this 
 }
+
+function startMusic() {
+    const audioPlayer = document.getElementById('audio-player');
+    // Set the desired start time in seconds
+    const startTimeInSeconds = 50; // 2 minutes
+    audioPlayer.currentTime = startTimeInSeconds;
+    audioPlayer.play();
+  }
