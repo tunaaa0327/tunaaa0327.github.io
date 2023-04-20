a6102350efa3458787564374f05101460b3c8c76
myCode <jules27jules@gmail.con>
2023-04-20 21:36:35 +0800

    Floating Images

--- sample.js
+++ sample.js
@@ -1,9 +1,13 @@
 function showInputs() {
     document.getElementById("luvu").style.display = 'block';
+
     var input1 = document.getElementById('input1').value;
     var output = document.getElementById('output');
     output.innerHTML = input1;
     startMusic();
+    document.getElementById("floatimage").style.display = 'block';
+    hearts();
+
 }
 
 function startMusic() {
@@ -13,4 +17,26 @@ function startMusic() {
     audioPlayer.currentTime = startTimeInSeconds;
     audioPlayer.play();
     //sample push one
-  }
+}
+
+function hearts(){
+    container = document.querySelector('.container')
+    const creat = document.createElement('div');
+    creat.classList.add('hearts');
+    creat.innerHTML = `🍟` + `🍗` + `🍔`;
+    creat.style.left = Math.random() * 100 + 'vw';
+ 
+    creat.style.animationDuration = Math.random() * 3 + 2 + 's';
+
+    container.appendChild(creat);
+
+    setTimeout(() =>
+    {
+        creat.remove();
+    },3000);
+
+}
+setInterval(hearts,80);
+
+
+
